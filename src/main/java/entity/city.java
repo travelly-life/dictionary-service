931253c28efd6bd9d6109783b82037f7c50ca842
srooodg <unitsin@yandex.ru>
2023-07-13 08:50:29 +0300

    создание таблиц со странами и городами и регионами

---
+++ src/main/java/entity/city.java
@@ -0,0 +1,32 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@Table(name = "city")
+public class city {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue
+    private BigInteger id;
+
+    @Column(name = "region_id")
+    @GeneratedValue
+    private BigInteger region_id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "crt_date")
+    private Timestamp crt_date;
+}
